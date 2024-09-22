cb7f2ca54eab424966e342aed3d1a4516eb0ccc4
strykeFreedom08 <jayralphmandanhuyan@gmail.com>
2024-09-21 21:39:13 -0400

    robofriends project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import CardList from './CardList';
+import Searchbox from "./Searchbox";
+import { robots } from './robots';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobot = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <Searchbox searchChange={this.onSearchChange.bind(this)} />
+                <CardList robots = {filteredRobot} />
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Apolinario Mabini',
+        username: 'ApoM',
+        email: 'ApoM@brainrevo.org'
+    },
+    {
+        id: 2,
+        name: 'Jose Rizal',
+        username: 'JRiz',
+        email: 'JRiz@makabayan.tv'
+    },
+    {
+        id: 3,
+        name: 'Emilio Aguinaldo',
+        username: 'AguilaE',
+        email: 'AguilaE@frpres.net'
+    },
+    {
+        id: 4,
+        name: 'Andres Bonifacio',
+        username: 'BonifiedA',
+        email: 'BonifiedA@farevo.edu'
+    },
+    {
+        id: 5,
+        name: 'Marcelo del Pilar',
+        username: 'PillarM',
+        email: 'PillarM@reformisted.info'
+    },
+    {
+        id: 6,
+        name: 'Antonio Luna',
+        username: 'AnthonyMoon',
+        email: 'AnthonyMoon@genfierce.ninja'
+    },
+    {
+        id: 7,
+        name: 'Juan Luna',
+        username: 'MoonJohn',
+        email: 'MoonJohn@painteract.buzz'
+    },
+    {
+        id: 8,
+        name: 'Melchora Aquino',
+        username: 'MelchoreA',
+        email: 'MelchoreA@revowoman.market'
+    },
+    {
+        id: 9,
+        name: 'Lapu Lapu',
+        username: 'Lapu2',
+        email: 'Lapu2@firsthero.ph'
+    },
+    {
+        id: 10,
+        name: 'Sultan Kudarat',
+        username: 'SoulK',
+        email: 'SoulK@mindadef.shop'
+    }
+]
