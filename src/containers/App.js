5df3303658a6df5ea67ae60053cebfd8efda7877
strykeFreedom08 <jayralphmandanhuyan@gmail.com>
2024-09-22 20:21:10 -0400

    robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from '../components/CardList';
 import Searchbox from "../components/Searchbox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css'
 
 class App extends Component {
@@ -36,7 +37,9 @@ class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <Searchbox searchChange={this.onSearchChange.bind(this)} />
                     <Scroll>
-                        <CardList robots = {filteredRobot} />
+                        <ErrorBoundry>
+                            <CardList robots = {filteredRobot} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
