74e8c659e215be2bbd55954804ea1e2105d8cffe
strykeFreedom08 <jayralphmandanhuyan@gmail.com>
2025-01-19 21:33:24 -0500

    create a test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './containers/App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Card from './components/Card';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  render(<Card />);
+  const imageElement = screen.getByRole('img');
+  
+  expect(imageElement).toBeInTheDocument();
+  expect(imageElement).toHaveAttribute('src');
+  expect(imageElement.src).not.toBe('');
+});
